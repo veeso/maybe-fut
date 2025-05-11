81237166586f1c4c31dc70f9441ee32337c30a2b
veeso <christian.visintin@veeso.dev>
2025-05-11 13:46:24 +0200

    fix: Changed unwrap trait names from "async" to "tokio"

--- maybe-fut/src/unwrap.rs
+++ maybe-fut/src/unwrap.rs
@@ -7,41 +7,41 @@
 /// Every type implemented by the **maybe_fut** library has a corresponding `Unwrap` implementation.
 pub trait Unwrap {
     type StdImpl;
-    type AsyncImpl;
+    type TokioImpl;
 
     /// Unwraps the std underlying implementation of the MaybeFut type.
     fn unwrap_std(self) -> Self::StdImpl;
 
-    /// Unwraps the async underlying implementation of the MaybeFut type.
-    fn unwrap_async(self) -> Self::AsyncImpl;
+    /// Unwraps the tokio underlying implementation of the MaybeFut type.
+    fn unwrap_tokio(self) -> Self::TokioImpl;
 
     /// Unwraps the std underlying implementation of the MaybeFut type as a reference.
     fn unwrap_std_ref(&self) -> &Self::StdImpl;
 
-    /// Unwraps the async underlying implementation of the MaybeFut type as a reference.
-    fn unwrap_async_ref(&self) -> &Self::AsyncImpl;
+    /// Unwraps the tokio underlying implementation of the MaybeFut type as a reference.
+    fn unwrap_tokio_ref(&self) -> &Self::TokioImpl;
 
     /// Unwraps the std underlying implementation of the MaybeFut type as a mutable reference.
     fn unwrap_std_mut(&mut self) -> &mut Self::StdImpl;
 
-    /// Unwraps the async underlying implementation of the MaybeFut type as a mutable reference.
-    fn unwrap_async_mut(&mut self) -> &mut Self::AsyncImpl;
+    /// Unwraps the tokio underlying implementation of the MaybeFut type as a mutable reference.
+    fn unwrap_tokio_mut(&mut self) -> &mut Self::TokioImpl;
 
     /// Safely unwraps the std underlying implementation of the MaybeFut type.
     fn get_std(self) -> Option<Self::StdImpl>;
 
-    /// Safely unwraps the async underlying implementation of the MaybeFut type.
-    fn get_async(self) -> Option<Self::AsyncImpl>;
+    /// Safely unwraps the tokio underlying implementation of the MaybeFut type.
+    fn get_tokio(self) -> Option<Self::TokioImpl>;
 
     /// Safely unwraps the std underlying implementation of the MaybeFut type as a reference.
     fn get_std_ref(&self) -> Option<&Self::StdImpl>;
 
-    /// Safely unwraps the async underlying implementation of the MaybeFut type as a reference.
-    fn get_async_ref(&self) -> Option<&Self::AsyncImpl>;
+    /// Safely unwraps the tokio underlying implementation of the MaybeFut type as a reference.
+    fn get_tokio_ref(&self) -> Option<&Self::TokioImpl>;
 
     /// Safely unwraps the std underlying implementation of the MaybeFut type as a mutable reference.
     fn get_std_mut(&mut self) -> Option<&mut Self::StdImpl>;
 
-    /// Safely unwraps the async underlying implementation of the MaybeFut type as a mutable reference.
-    fn get_async_mut(&mut self) -> Option<&mut Self::AsyncImpl>;
+    /// Safely unwraps the tokio underlying implementation of the MaybeFut type as a mutable reference.
+    fn get_tokio_mut(&mut self) -> Option<&mut Self::TokioImpl>;
 }
